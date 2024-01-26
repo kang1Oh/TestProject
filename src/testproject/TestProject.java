fa4e95f1cf726cf0d9a3911e2f26b51cc7d2ed14
kang1Oh <kangteoh19@gmail.com>
2024-01-26 16:48:10 +0800

    Modified TestProject

--- src/testproject/TestProject.java
+++ src/testproject/TestProject.java
@@ -4,6 +4,8 @@
  */
 package testproject;
 
+import java.util.Scanner;
+
 /**
  *
  * @author USePObrero_User595
@@ -14,7 +16,16 @@ public class TestProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Hello, Humans! Press yes to learn of our message.");
+        String answer = in.next();
+        
+        if(answer.equalsIgnoreCase("yes")){
+            System.out.println("Oops! You've just given us consent to take your rights, and you will all be captured in 3-4 business days. Thanks!");
+        }else{
+            System.out.println("Understandable, have a nice day!");
+        }
     }
     
 }
